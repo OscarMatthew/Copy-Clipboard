68b91e475d6f5879d4779a83eb7b18506aaabb65
Raman Matthew <793raman@gmail.com>
2019-09-05 11:10:15 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 // Put your javascript or jquery here.
 // Remember to comment well. Sections, which page it's used on, and what each function does, and in most cases what each line does.
-
 new ClipboardJS('.btn');
 
 var clipboard = new ClipboardJS('.btn');
 
+clipboard.on('success', function(e) {
+  alert("Copied");
+});
+
 $(document).ready(function () {
-  var currentUrl = document.URL;
-  $("#input-url").val(currentUrl);
+  var $currentUrl = document.URL;
+  $("#input-url").val($currentUrl);
 });
